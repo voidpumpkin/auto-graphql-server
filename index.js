fb5b5a6d5eae00b29eeda2874ea0b779164744ae
Julius Lungys <32368314+voidpumpkin@users.noreply.github.com>
2021-02-03 13:25:19 +0200

    add koa

---
+++ index.js
@@ -0,0 +1,10 @@
+import Koa from "koa";
+const app = new Koa();
+
+app.use(async (ctx) => {
+  ctx.body = "Hello World";
+});
+
+let PORT = 3000;
+console.log(`Listening at http://localhost:${PORT}`);
+app.listen(PORT);
