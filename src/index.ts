83b63e7679f8319726dd1932f7ba3eb0545efc55
Julius Lungys <32368314+voidpumpkin@users.noreply.github.com>
2021-02-04 19:58:43 +0200

    add migration example

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,63 @@
 import Koa from 'koa';
+import Knex from 'knex';
+
 const app = new Koa();
+const knex = Knex({
+    client: 'sqlite3',
+    connection: {
+        filename: './test.sqlite3',
+    },
+    useNullAsDefault: true,
+});
+
+(async () => {
+    const copyTable = async (from: string, to: string): Promise<void> => {
+        const ids = await knex(from).select('id');
+        await Promise.all(
+            ids.map(async ({ id }) => {
+                const entry = await knex(from).select().where('id', id);
+                await knex(to).insert(entry);
+            })
+        );
+    };
+
+    if (!(await knex.schema.hasTable('users'))) {
+        await knex.schema.createTable('users', (tableBuilder) => {
+            tableBuilder.increments();
+            tableBuilder.string('firstname');
+            tableBuilder.string('lastname');
+            tableBuilder.integer('age');
+        });
+        await knex('users').insert([
+            { firstname: 'bob', lastname: 'rogers', age: 25 },
+            { firstname: 'sammy', lastname: 'rogers', age: 23 },
+            { firstname: 'al', lastname: 'rogers', age: 40 },
+            { firstname: 'smit', lastname: 'chocolate', age: 60 },
+        ]);
+    } else {
+        await knex.schema.createTable('TEMP_NAME_ID_users', (tableBuilder) => {
+            tableBuilder.increments();
+            tableBuilder.string('firstname');
+            tableBuilder.string('lastname');
+            tableBuilder.integer('age');
+        });
+        await copyTable('users', 'TEMP_NAME_ID_users');
+        await knex.schema.dropTable('users');
+        await knex.schema.createTable('users', (tableBuilder) => {
+            tableBuilder.increments();
+            tableBuilder.string('firstname');
+            tableBuilder.string('lastname');
+            tableBuilder.string('middlename');
+            tableBuilder.integer('age');
+        });
+        await copyTable('TEMP_NAME_ID_users', 'users');
+        await knex.schema.dropTable('TEMP_NAME_ID_users');
+    }
+})();
 
 app.use(async (ctx) => {
-    ctx.body = 'Hello World';
+    const users = await knex.select().table('users');
+    ctx.body = { users };
 });
 
 const PORT = 3000;
