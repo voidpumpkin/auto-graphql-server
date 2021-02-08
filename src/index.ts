d107c839e18107b69b3bf8b33062b6daaa2da65e
Julius Lungys <32368314+voidpumpkin@users.noreply.github.com>
2021-02-08 14:33:26 +0200

    knex (#1)
    * switch to ts-node
    
    * add migration example
    
    * add diff proof of concept

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,59 @@
 import Koa from 'koa';
+import Knex, { CreateTableBuilder } from 'knex';
+import { diff } from 'rus-diff';
+
+type BuilderMethod = keyof CreateTableBuilder;
+type Table = Record<string, BuilderMethod[]>;
+type Schema = Record<string, Table>;
+
+const OLD_SCHEMA: Schema = {
+    users: {
+        id: ['increments'],
+        firstname: ['string'],
+        lastname: ['string'],
+        age: ['integer'],
+    },
+};
+const NEW_SCHEMA: Schema = {
+    users: {
+        id: ['increments'],
+        firstname: ['string'],
+        lastname: ['string'],
+        middlename: ['string'],
+        age: ['integer'],
+    },
+};
+console.log('Proof of concept for schema diff', diff(OLD_SCHEMA, NEW_SCHEMA));
+
 const app = new Koa();
+const knex = Knex({
+    client: 'sqlite3',
+    connection: {
+        filename: './test.sqlite3',
+    },
+    useNullAsDefault: true,
+});
+
+(async () => {
+    if (!(await knex.schema.hasTable('users'))) {
+        await knex.schema.createTable('users', (tableBuilder) => {
+            tableBuilder.increments();
+            tableBuilder.string('firstname');
+            tableBuilder.string('lastname');
+            tableBuilder.integer('age');
+        });
+        await knex('users').insert([
+            { firstname: 'bob', lastname: 'rogers', age: 25 },
+            { firstname: 'sammy', lastname: 'rogers', age: 23 },
+            { firstname: 'al', lastname: 'rogers', age: 40 },
+            { firstname: 'smit', lastname: 'chocolate', age: 60 },
+        ]);
+    }
+})();
 
 app.use(async (ctx) => {
-    ctx.body = 'Hello World';
+    const users = await knex.select().table('users');
+    ctx.body = { users };
 });
 
 const PORT = 3000;
