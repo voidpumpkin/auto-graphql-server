dfa041dca071aa1357dbf14b397ccc1f74cd7fd2
Julius Lungys <32368314+voidpumpkin@users.noreply.github.com>
2021-04-12 15:27:15 +0300

    fix was not throwing if not object query type

--- src/schema/validateQueryType.ts
+++ src/schema/validateQueryType.ts
@@ -1,10 +1,12 @@
 import { GraphQLObjectType, isObjectType } from 'graphql';
+//import { validateQueryFields } from './validateQueryFields';
 
 export function validateQueryType(queryType: undefined | null | GraphQLObjectType): void {
     if (!queryType) {
         return;
     }
     if (!isObjectType(queryType)) {
-        Error('query type has to be an object');
+        throw Error('query type has to be an object');
     }
+    //validateQueryFields(queryType);
 }
