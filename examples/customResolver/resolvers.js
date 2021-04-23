27a25b9b2e07ef13ce4e46731a5cddb6646c02b5
Julius Lungys <32368314+voidpumpkin@users.noreply.github.com>
2021-04-23 20:39:42 +0300

    add custom resolvers example

---
+++ examples/customResolver/resolvers.js
@@ -0,0 +1,9 @@
+module.exports = {
+    Query: {
+        books: function (knex) {
+            return async function (root, args, context, info) {
+                return await knex('Book').select().where(args);
+            };
+        },
+    },
+};
