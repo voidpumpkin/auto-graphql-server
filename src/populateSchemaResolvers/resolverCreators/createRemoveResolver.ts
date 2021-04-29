0617483a6b269472d21c8eebf9a24618be17a925
Julius Lungys <32368314+voidpumpkin@users.noreply.github.com>
2021-04-29 13:00:53 +0300

    fix on remove delete parent relationship

--- src/populateSchemaResolvers/resolverCreators/createRemoveResolver.ts
+++ src/populateSchemaResolvers/resolverCreators/createRemoveResolver.ts
@@ -1,7 +1,9 @@
 import Knex from 'knex';
-import { NO_TABLE, PARENTS_LIST } from '../../directives/directives';
+import { NO_TABLE } from '../../directives/directives';
 import { hasDirectives } from '../../directives/hasDirectives';
 import { IFieldResolver } from 'graphql-tools';
+import { getParentFieldValue } from '../../directives/getParentFieldValue';
+import { getParentListDirective } from '../../directives/getParentListDirective';
 
 export function createRemoveResolver(
     knex: Knex,
@@ -13,11 +15,19 @@ export function createRemoveResolver(
         const knexResult = await knex(returnTypeName).select().where(filter);
         const results = knexResult.map((r: all) => r?.id);
         await Promise.all(
-            Object.values(listFields).map(async ({ astNode, name }) => {
-                if (hasDirectives(astNode, [NO_TABLE, PARENTS_LIST])) {
+            Object.values(listFields).map(async (field) => {
+                const parentDirective = getParentListDirective(field.astNode);
+                if (parentDirective) {
+                    const parentFieldValue = getParentFieldValue(parentDirective);
+                    const listTableName = `__${field.type.ofType.name}_${parentFieldValue}_list`;
+                    const foreignKeyName = `${field.type.ofType.name}_${parentFieldValue}_${returnTypeName}_id`;
+                    await knex(listTableName).whereIn(foreignKeyName, results).delete();
                     return;
                 }
-                const listTableName = `__${returnTypeName}_${name}_list`;
+                if (hasDirectives(field.astNode, [NO_TABLE])) {
+                    return;
+                }
+                const listTableName = `__${returnTypeName}_${field.name}_list`;
                 await knex(listTableName).whereIn(`${returnTypeName}_id`, results).delete();
             })
         );
